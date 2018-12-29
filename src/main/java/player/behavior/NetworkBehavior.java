b125a6d522daad5b55ec3c07a46b92bba1c163c9
cymrucoder <git.cmt@gmail.com>
2018-12-29 21:14:23 +0000

    Revert old network behavior to original code.

--- src/main/java/player/behavior/NetworkBehavior.java
+++ src/main/java/player/behavior/NetworkBehavior.java
@@ -2,16 +2,12 @@ package player.behavior;
 
 import blarg.chess.Board;
 import blarg.chess.Move;
-import blarg.chess.Piece;
+import blarg.newnet.Layer;
 import blarg.newnet.Network;
+import blarg.newnet.Neuron;
 import blarg.newnet.ValueTracker;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -32,44 +28,38 @@ public class NetworkBehavior extends PlayerBehavior {
     }
     
     private void setupNetwork() {
-        try {
-            network = new Network(new FileReader("networks\\royalphess1.csv"));
-            
-//        Layer inputLayer = new Layer();
-//        Neuron friendlyPawnCountNeuron = new Neuron();
-//        friendlyPawnCountNeuron.addConnection(0);
-//        friendlyPawnCountNeuron.setUseActivationFunction(true);
-//        Neuron enemyPawnCountNeuron = new Neuron();
-//        enemyPawnCountNeuron.addConnection(1);
-//        enemyPawnCountNeuron.setUseActivationFunction(true);
-//        inputLayer.addNeuron(friendlyPawnCountNeuron);
-//        inputLayer.addNeuron(enemyPawnCountNeuron);
-//        
-////        Layer middleLayer = new Layer();
-////        Neuron middleNeuronUpper = new Neuron(0.5, 0.5, 0.5);
-////        Neuron middleNeuronLower = new Neuron(0.5, 0.5, 0.5);
-////        middleLayer.addNeuron(middleNeuronUpper);
-////        middleLayer.addNeuron(middleNeuronLower);
-//        
-//        Layer outputLayer = new Layer();
-//        //Neuron pieceToMoveNeuron = new Neuron(0.5, 0.5, 0.5);
-//        //Neuron moveToMakeNeuron = new Neuron(0.5, 0.5, 0.5);
-//        //outputLayer.addNeuron(pieceToMoveNeuron);
-//        //outputLayer.addNeuron(moveToMakeNeuron );
-//        Neuron shouldCaptureNeuron = new Neuron();
-//        shouldCaptureNeuron.addConnection(0);
-//        shouldCaptureNeuron.addConnection(1);
-//        shouldCaptureNeuron.setUseActivationFunction(true);
-//        outputLayer.addNeuron(shouldCaptureNeuron);
-//        
-//        network.addLayer(inputLayer);
-//        //network.addLayer(middleLayer);
-//        network.addLayer(outputLayer);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(NetworkBehavior.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(NetworkBehavior.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        network = new Network();
+        
+        Layer inputLayer = new Layer();
+        Neuron friendlyPawnCountNeuron = new Neuron();
+        friendlyPawnCountNeuron.addConnection(0);
+        friendlyPawnCountNeuron.setUseActivationFunction(true);
+        Neuron enemyPawnCountNeuron = new Neuron();
+        enemyPawnCountNeuron.addConnection(1);
+        enemyPawnCountNeuron.setUseActivationFunction(true);
+        inputLayer.addNeuron(friendlyPawnCountNeuron);
+        inputLayer.addNeuron(enemyPawnCountNeuron);
+        
+//        Layer middleLayer = new Layer();
+//        Neuron middleNeuronUpper = new Neuron(0.5, 0.5, 0.5);
+//        Neuron middleNeuronLower = new Neuron(0.5, 0.5, 0.5);
+//        middleLayer.addNeuron(middleNeuronUpper);
+//        middleLayer.addNeuron(middleNeuronLower);
+        
+        Layer outputLayer = new Layer();
+        //Neuron pieceToMoveNeuron = new Neuron(0.5, 0.5, 0.5);
+        //Neuron moveToMakeNeuron = new Neuron(0.5, 0.5, 0.5);
+        //outputLayer.addNeuron(pieceToMoveNeuron);
+        //outputLayer.addNeuron(moveToMakeNeuron );
+        Neuron shouldCaptureNeuron = new Neuron();
+        shouldCaptureNeuron.addConnection(0);
+        shouldCaptureNeuron.addConnection(1);
+        shouldCaptureNeuron.setUseActivationFunction(true);
+        outputLayer.addNeuron(shouldCaptureNeuron);
+        
+        network.addLayer(inputLayer);
+        //network.addLayer(middleLayer);
+        network.addLayer(outputLayer);
     }
     
     @Override
@@ -92,62 +82,6 @@ public class NetworkBehavior extends PlayerBehavior {
         ValueTracker inputs = new ValueTracker();
         inputs.add(0, (double) friendlyPawns / 8.0);
         inputs.add(1, (double) enemyPawns / 8.0);
-        
-        double canCapturePawns = 0.0;
-        double canPawnsBeCaptured = 0.0;
-        double canCaptureKing = 0.0;
-        double kingInCheck = 0.0;
-        
-        for (Move move : candidateMoves) {
-            String thisType = board.getPieces()[move.getOldX()][move.getOldY()].getType();
-            String typeInNewSquare;
-            
-            if (board.getPieces()[move.getNewX()][move.getNewY()] == null) {
-                typeInNewSquare = "";
-            } else {
-                typeInNewSquare = board.getPieces()[move.getNewX()][move.getNewY()].getType();
-            }
-            
-            if (Piece.PAWN.equals(thisType) && Piece.PAWN.equals(typeInNewSquare)) {
-                canCapturePawns = 1.0;
-            }
-            
-            if (Piece.KING.equals(typeInNewSquare)) {
-                canCaptureKing = 1.0;
-            }
-        }
-            
-        int enemyColor = Piece.WHITE;
-            
-        if (board.getCurrentTurnColor() == Piece.WHITE) {
-            enemyColor = Piece.BLACK;
-        } 
-            
-        List<Move> enemyMoves = board.getMoveCandidates(enemyColor);
-        
-        for (Move move : enemyMoves) {
-            String enemyType = board.getPieces()[move.getOldX()][move.getOldY()].getType();
-            String typeInNewSquare;
-            
-            if (board.getPieces()[move.getNewX()][move.getNewY()] == null) {
-                typeInNewSquare = "";
-            } else {
-                typeInNewSquare = board.getPieces()[move.getNewX()][move.getNewY()].getType();
-            }
-            
-            if (Piece.PAWN.equals(enemyType) && Piece.PAWN.equals(typeInNewSquare)) {
-                canPawnsBeCaptured = 1.0;
-            }
-            
-            if (Piece.KING.equals(typeInNewSquare)) {
-                kingInCheck = 1.0;
-            }
-        }
-        
-        inputs.add(2, canCapturePawns);
-        inputs.add(3, canPawnsBeCaptured);
-        inputs.add(4, canCaptureKing);
-        inputs.add(5, kingInCheck);
         
         ValueTracker outputs = network.process(inputs);
         //System.out.println("Output " + outputs.get(0));
